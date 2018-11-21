06be177a9679021febf7cdea95cb406390272d55
Carter <chalfmann9547@myhta.org>
2018-11-21 12:22:28 -1000

    donee

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,29 @@
 module.exports = function findTheLongestWord(sen) {
-  // write your code in here
+  
+ var longestlength=0;
+ var longestWord='';
+
+sen.map(word => {
+  //iterates through the array
+if(word.length>longestlength) {
+  //checks if current word is longer than the current longest word
+longestlength=word.length;
+//if so, it changes the longestlength to the length of that word...
+longestWord=word;
+//...and the longest word to the word
+}
+})
+return longestWord;
+//returns the longest word
+
+
+
+
+
+  
+
+
+
+
+
 }
